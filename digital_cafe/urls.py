5c0a15309a3c895a53aee3896333acaad7c27f2f
Chessie Trillana <ana.trillana@student.ateneo.edu>
2026-09-13 23:17:05 +0800

    feat: wire up /accounts/signup/

--- digital_cafe/urls.py
+++ digital_cafe/urls.py
@@ -19,8 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from cafe import views as cafe_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/signup/', cafe_views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('cafe.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
