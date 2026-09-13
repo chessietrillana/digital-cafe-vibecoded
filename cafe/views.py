766d57ebeaabaee0fde852152364eb9bd0ad9e4b
Chessie Trillana <ana.trillana@student.ateneo.edu>
2026-09-13 21:47:11 +0800

    feat: add home page listing active products

--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+@login_required
+def home(request):
+    products = Product.objects.filter(is_active=True).order_by("name")
+    greeting_name = request.user.first_name or request.user.username
+    return render(
+        request,
+        "cafe/home.html",
+        {"products": products, "greeting_name": greeting_name},
+    )
